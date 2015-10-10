bfaaf21b69d7cb4dc908f0c74234b32040706c8e
Veronica <veronica.xiaoyu@gmail.com>
2015-10-10 00:22:32 -0400

    The FPU.java is modified. Although it cannot test, it has all logics we need.

--- ComputerArchitectureTomsuloSimulator/src/cs/architecture/FPU.java
+++ ComputerArchitectureTomsuloSimulator/src/cs/architecture/FPU.java
@@ -18,4 +18,66 @@ public class FPU {
 			instance = new FPU();
 		return instance;
 	}
+	
+	
+	public boolean insertInstruction(String opco, int rs, int rt, int rd){
+
+		for(int i=0;i<ReservationStationNumber;i++){
+			if((!ReservationStation[i].busy)&&(ROB.size < max)){
+				if(RegisterStatus[rs].busy){
+					//Update Status table
+					h = RegisterStatus[rs].Reorder;
+					if(ROB[h].Ready){
+						ReservationStation[i].Vj = ROB[h].Value;
+						ReservationStation[i].Qj = 0;
+					}else{
+						ReservationStation[i].Qj = h;
+					}
+					
+				}else{
+					ReservationStation[i].Vj = Regs[rs];
+					ReservationStation[i].Qj = 0;
+				}
+				
+				if(RegisterStatus[rt].busy){
+					//Update Status table
+					h = RegisterStatus[rt].Reorder;
+					if(ROB[h].Ready){
+						ReservationStation[i].Vk = ROB[h].Value;
+						ReservationStation[i].Qk = 0;
+					}else{
+						ReservationStation[i].Qk = h;
+					}
+					
+				}else{
+					ReservationStation[i].Vk = Regs[rt];
+					ReservationStation[i].Qk = 0;
+				}
+				
+				
+				ReservationStation[i].busy = true;
+				ReservationStation[i].dest = b;
+				ROB[b].Instruction = opco;
+				ROB[b].Dest = rd;
+				ROB[b].Ready = false;
+				return true;
+			}
+		}
+		// the issue is not successful, needs to stall for one cycle.
+		return false;
+	}
+	
+	void computeResult(int input){
+		//change the long bits to double to calculate the result
+		double rs = Double.longBitsToDouble(ReservationStation[input].Vj);
+		double rt = Double.longBitsToDouble(ReservationStation[input].Vk);
+		double result = 0;
+		
+		//calculate the result
+		if(ReservationStation[input].operation.equals("mul.d"))
+			result = rs * rt;
+		
+		//change the result to long bits
+		ReservationStation[input].result = Double.doubleToLongBits(result);
+	}
 }
