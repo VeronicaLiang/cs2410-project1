0eec2d6a8ed05631dcd7f047f243a394901ce8c9
Xiaoyu <veronica.xiaoyu@gmail.com>
2015-10-16 16:12:29 -0400

    Haven't finished the execute stage

--- ProjectTwo/src/LoadStore.java
+++ ProjectTwo/src/LoadStore.java
@@ -10,9 +10,9 @@ import java.util.Map.Entry;
 
 /*
  * Unit       Latency for operation         Reservation stations            Instructions executing on the unit
- * 
+ *
  * Load/Store 1 for address calculation     3 load buffer +3 store buffer   LD, L.D, SD, S.D
- * 
+ *
  */
 public class LoadStore {
 	private static LoadStore instance;
@@ -22,8 +22,8 @@ public class LoadStore {
 			instance = new LoadStore();
 		return instance;
 	}
-private static final int LATENCY = 5;
-	
+	private static final int LATENCY = 5;
+
 	/*
 	 * Reservation Stations Table.
 	 * Station 1 to 4 are INT0&INT1 stations.
@@ -33,17 +33,51 @@ private static final int LATENCY = 5;
 	   Station 18 and 19 are BU  stations.
 	 */
 	public boolean insertInstruction(Instruction instruction){
-		for(int i =7;i<=12;i++){
+		int start;
+		int end;
+		if(instruction.opco.contains("L")){
+			start = 7;
+			end = 9;
+		}else{
+			start = 10;
+			end = 12;
+		}
+		for(int i =start;i<=end;i++){
 			Station station = (Station) Const.reservationStations.get(i+"");
+
 			if((!station.Busy)){
 				int h;//TODO 这里的h是ROB的head entry？？？
 				Register register;
-				if (instruction.rs.contains("R")){
-					register = (Register) Const.integerRegistersStatus.get(instruction.rs);
+				String rs;
+				String rd;
+				int replacement = 0;
+				int rd_replacement = 0;
+				// Check whether there is a replacement
+				if(instruction.rs.contains("(")){
+					String [] tmp = instruction.rs.split("\\(");
+					replacement = Integer.parseInt(tmp[0]);
+					tmp = tmp[1].split("\\)");
+					rs = tmp[0]+"";
 				}else{
-					register = (Register) Const.floatRegistersStatus.get(instruction.rs);
+					rs = instruction.rs;
 				}
-				
+
+				if(instruction.rd.contains("(")){
+					String [] tmp = instruction.rd.split("\\(");
+					rd_replacement = Integer.parseInt(tmp[0]);
+					tmp = tmp[1].split("\\)");
+					rd = tmp[0]+"";
+				}else{
+					rd = instruction.rd;
+				}
+
+				//all instructions need to check whether RegisterStat[rs] is busy
+				if (rs.contains("R")){
+					register = (Register) Const.integerRegistersStatus.get(rs);
+				}else{
+					register = (Register) Const.floatRegistersStatus.get(rs);
+				}
+
 				if(register.busy){
 					h = register.Reorder;
 					if(((ROBItem)Const.ROB.get(h)).ready){
@@ -56,123 +90,99 @@ private static final int LATENCY = 5;
 					station.Vj = register.value;
 					station.Qj = 0;
 				}
-				
-				
-				
+
 				if(instruction.opco.equals("LD") || instruction.opco.equals("L.D")){
 					ROBItem item = new ROBItem();
-					item.destination = instruction.rt;
+					item.instruction = instruction;
+					item.destination = instruction.rd;
 					Const.ROB.add(item);
 					int b = Const.ROB.indexOf(item);
-					register.Reorder = b; 
+					register.Reorder = b;
 					register.busy = true;
-					station.A = instruction.rs;
+					// replacement + rs  is the address ;
+					station.A = replacement + (int)register.value;
 					station.loadFlag = 1;
 				}else{
-					// The same update for rt 
-					if (instruction.rt.contains("R")){
-						register = (Register) Const.integerRegistersStatus.get(instruction.rt);
-					}else{
-						register = (Register) Const.floatRegistersStatus.get(instruction.rt);
-					}
-					
-					if(register.busy){
-						h = register.Reorder;
-						if(((ROBItem)Const.ROB.get(h)).ready){
-							station.Vk = ((ROBItem)Const.ROB.get(h)).value;
-							station.Qk = 0;
-						}else{
-							station.Qk = h;
-						}
-					}else{
-						station.Vk = register.value ;
-						station.Qk = 0;
-					}
-					
-					if (instruction.rt.contains("R")){
-						register = (Register) Const.integerRegistersStatus.get(instruction.rd);
-					}else{
-						register = (Register) Const.floatRegistersStatus.get(instruction.rd);
-					}
-					station.A = instruction.rs;
+					station.A = rd_replacement + Integer.parseInt(rd);
 				}
-				
-				
-				
+				station.Busy = true;
+				station.latency = 0;
 				return true;
 			}
 		}
-		
+
 		// the issue is not successful, needs to stall for one cycle.
 		return false;
 	}
-	
+
 	public void execute(){
 		for(int i = 7;i<=12;i++){
 			Station station = (Station) Const.reservationStations.get(i+"");
-			if((station.latency>0) && (station.latency<LATENCY)){
-				station.latency = station.latency +1;
-			}else{
-				if(station.latency==LATENCY){
-					//Write result. 
-					if(station.loadFlag==0){
-						if(station.Qk == 0){
-							((ROBItem)Const.ROB.get(0)).value = station.Vk;
+			if(station.Busy){
+				if((station.latency>0) && (station.latency<LATENCY)){
+					station.latency = station.latency +1;
+				}else{
+					if(station.latency>=LATENCY){
+						//Write result.
+						if(station.loadFlag==0){
+							if(station.Qk == 0){
+								((ROBItem)Const.ROB.get(0)).value = station.Vk;
+							}
+						}else{
+							int b = station.Dest;
+							station.Busy = false;
+
+							Iterator iterator = Const.reservationStations.entrySet().iterator();
+							while(iterator.hasNext()){
+								Map.Entry entry = (Entry) iterator.next();
+								Station s = (Station) entry.getValue();
+								if(s.Qj==b){
+									s.Vj = station.result;s.Qj = 0;
+								}
+								if(s.Qk==b){
+									s.Vk = station.result;s.Qk = 0;
+								}
+								((ROBItem)Const.ROB.get(b)).value = station.result;
+								((ROBItem)Const.ROB.get(b)).ready = true;
+							}
 						}
 					}else{
-						int b = station.Dest;
-						station.Busy = false;
-						
-						Iterator iterator = Const.reservationStations.entrySet().iterator();
-						while(iterator.hasNext()){
-						   Map.Entry entry = (Entry) iterator.next();
-						   Station s = (Station) entry.getValue();
-						   if(s.Qj==b){
-							   s.Vj = station.result;s.Qj = 0;
-						   }
-						   if(s.Qk==b){
-							   s.Vk = station.result;s.Qk = 0;
-						   }
-						   ((ROBItem)Const.ROB.get(b)).value = station.result;
-						   ((ROBItem)Const.ROB.get(b)).ready = true;
-						}
-					}
-				}else{
-					
-					if((station.Qj==0)){
-						if(station.loadFlag>0){//Load operation
-							if(station.loadFlag ==1){
-								boolean isStoreAhead = false;
-								for(int n = (station.Dest-1);n>=0;n--){
-									if(((ROBItem)Const.ROB.get(n)).instruction.equals("LD") ||
-											((ROBItem)Const.ROB.get(n)).instruction.equals("L.D")){
-										isStoreAhead = true;
+
+						if((station.Qj==0)){
+							if(station.loadFlag>0){//Load operation
+								if(station.loadFlag ==1){
+									boolean isStoreAhead = false;
+									for(int n = (station.Dest-1);n>=0;n--){
+										if(((ROBItem)Const.ROB.get(n)).instruction.equals("LD") ||
+												((ROBItem)Const.ROB.get(n)).instruction.equals("L.D")){
+											isStoreAhead = true;
+										}
 									}
-								}
-								if(!isStoreAhead){
-									station.loadFlag = 2;
-									station.A = station.Vj + station.A;
-								}
-								
-							}else if(station.loadFlag ==2){
-								//TODO read from memory station.A
-								station.latency = station.latency+1;
-							}
-						}else{//store operation
-							if(Const.ROB.size()>0){
-								ROBItem item = (ROBItem)Const.ROB.get(0);
-								if(item.instruction.equals("SD") || item.instruction.equals("S.D")){
-//									item.Address = station.Vj + station.A;
-									item.destination = station.Vj + station.A;//TODO 是不是用可以用一个别的？？？
+									if(!isStoreAhead){
+										station.loadFlag = 2;
+										station.A = station.Vj + station.A;
+									}
+
+								}else if(station.loadFlag ==2){
+									//TODO read from memory station.A
 									station.latency = station.latency+1;
 								}
+							}else{//store operation
+								if(Const.ROB.size()>0){
+									ROBItem item = (ROBItem)Const.ROB.get(0);
+									if(item.instruction.equals("SD") || item.instruction.equals("S.D")){
+//									item.Address = station.Vj + station.A;
+										item.destination = station.Vj + station.A;//TODO 是不是用可以用一个别的？？？
+										station.latency = station.latency+1;
+									}
+								}
 							}
+
 						}
-						
 					}
 				}
-				
 			}
 		}
 	}
 }
+
