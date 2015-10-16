7214923c1ac6a4ee660675d0c32090467e9d373f
Xiaoyu <veronica.xiaoyu@gmail.com>
2015-10-16 16:50:17 -0400

    Add another variable "address" in ROBItem.

--- ProjectTwo/src/ROBItem.java
+++ ProjectTwo/src/ROBItem.java
@@ -6,6 +6,7 @@ public class ROBItem {
 	Instruction instruction; // change the type of instruction, make the comparing of instruction type easier. --xl
 	String state;
 	String destination;
+	int address;
 	float value;
 	boolean ready = false;
 }
