909f3c36099e848fd651f3514e6b08ac614d5a29
Colin Zhang <me@colinzhang.com>
2015-10-16 15:46:34 -0400

    busy station

--- ProjectTwo/src/MULT.java
+++ ProjectTwo/src/MULT.java
@@ -90,6 +90,7 @@ private static final int LATENCY = 2;
 				int b = Const.ROB.indexOf(item);
 				register.Reorder = b; 
 				register.busy = true;
+				station.Busy = true;
 				station.latency = 0;
 				return true;
 			}
