761d77a5579406463e733a4a81e8aa365e2beac7
Xiaoyu <veronica.xiaoyu@gmail.com>
2015-10-16 20:48:05 -0400

    Memory, change

--- ProjectTwo/src/Memory.java
+++ ProjectTwo/src/Memory.java
@@ -3,6 +3,16 @@
 import java.util.*;
 
 public class Memory {
+
+	private static Memory instance;
+	private Memory (){
+	}
+	public static Memory getInstance(){
+		if(instance==null){
+			instance = new Memory();
+		}
+		return instance;
+	}
 	private static List instrs = new ArrayList(); // the array's index is for pc
 	private static Hashtable data = new Hashtable(); 
 	
@@ -18,7 +28,7 @@ public class Memory {
 		// when parse the string, hard coded... 
 		String[] records = line.split("\\t+");
 		String[] tmp = records[records.length-1].split("\\)\\s+\\=\\s+");
-		double number = Double.parseDouble(tmp[tmp.length-1]);
+		float number = Float.parseFloat(tmp[tmp.length-1]);
 		String[] tmp2 = tmp[0].split("\\(");
 		int index = Integer.parseInt(tmp2[tmp2.length-1]);
 		data.put(index, number);
