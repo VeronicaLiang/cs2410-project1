88532e18269498ed3d3cd24aa741b48a4888ba2c
youbehappy <youbehappy@qq.com>
2015-10-17 14:12:08 -0400

    buscount++

--- ProjectTwo/src/TomasuloSimulator.java
+++ ProjectTwo/src/TomasuloSimulator.java
@@ -147,7 +147,8 @@ public class TomasuloSimulator {
 			issue(DQueue);
 			
 			clock_cycle ++;
-			System.out.println(clock_cycle);
+//			System.out.println(clock_cycle);
+//			System.out.println("pc->"+pc+"  getInstrs->"+memory.getInstrs().size()+"  Const.lastOfROB->"+Const.lastOfROB+"  firstOfROB->"+Const.firstOfROB);
 			if(pc >= memory.getInstrs().size() && Const.lastOfROB - Const.firstOfROB == 0){
 				finishedFlag = true;
 			}
@@ -247,11 +248,14 @@ public class TomasuloSimulator {
        or memory destination is updated, eliminating the need for the ROB entry.
      */
     public void commit(BranchTargetBuffer btb){
+    	
 		int NC = 4;
 		int bus_count = 0;
+		
     	while(Const.lastOfROB - Const.firstOfROB > 0 && bus_count < NC ){
     		int h = Const.firstOfROB;  // always commit the first item in ROB
 			ROBItem item = (ROBItem)Const.ROB.get(h);
+			System.out.println("ttbus_count->"+bus_count);
     		if(item.ready){
     			String d = item.destination;
     			if(item.instruction.opco.equals("BEQZ") || item.instruction.opco.equals("BNEZ")
@@ -275,7 +279,7 @@ public class TomasuloSimulator {
 							pc = item.offset;
     				    }
     				}
-					
+    				bus_count++;
     			}else if(item.instruction.opco.equals("S.D") || item.instruction.opco.equals("SD")){
     				memory.getData().put(item.address, item.value);
 					bus_count++;
@@ -300,7 +304,7 @@ public class TomasuloSimulator {
         				((Register)Const.floatRegistersStatus.get(d)).busy = false;
         			}
     			}
-    			
+    			System.out.println("bus_count->"+bus_count);
     			
     		} else {
     			break;
